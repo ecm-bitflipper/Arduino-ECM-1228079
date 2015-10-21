01f5d6238e5fde478c46657fd956e4bbc6afb5f2
bitflipper <timekiller_9@hotmail.com>
2015-10-21 13:41:01 -0700

    Initial upload of Windows scripts to generate sound files

---
+++ MCSS/scripts/MALF.bat
@@ -0,0 +1,24 @@
+MD WAV
+MD WAV\MALF
+echo "Oxygen Sensor Circuit Failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF13.wav
+echo "Coolant sensor detects high temperature" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF14.wav
+echo "Coolant sensor detects low temperature" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF15.wav
+echo "Throttle Position Sensor detects high voltage" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF21.wav
+echo "Throttle Position Sensor detects low voltage" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF22.wav
+echo "The mixture control solenoid is open or grounded" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF23.wav
+echo "Vehicle Speed Sensor Failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF24.wav
+echo "Map Sensor circuit failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF33.wav
+echo "Vacuum or Map Sensor circuit failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF34.wav
+
+echo "Idle Speed Control failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF35.wav
+echo "No distributor reference pulses to the ECM" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF41.wav
+echo "Electronic Spark Timing bypass circuit is grounded or open" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF42.wav
+echo "The electronic spark control retard signal has been on for too long " | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF43.wav
+
+
+echo "Lean Exhaust" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF44.wav
+echo "Rich Exhaust" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF45.wav
+echo "Prawm Error" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF51.wav
+echo "EGR valve failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF53.wav
+echo "The mixture control solenoid voltage is high" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF54.wav
+echo "ECM failure" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF55.wav
---
+++ MCSS/scripts/responses.bat
@@ -0,0 +1,36 @@
+MD WAV
+echo "Yes Driver" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\TRIGGER.wav
+echo "Current vehicle speed is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\SPEED1.wav
+echo "miales per hour" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\SPEED2.wav
+echo "Current engine R P M is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\RPM1.wav
+echo "R P M" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\RPM2.wav
+echo "Current throttle position sensor reading is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\TPS1.wav
+echo "volts" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\TPS2.wav
+echo "I will notify you each time we reach wide open throttle" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\TPS3.wav
+echo "Wide Open Throttle" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\WOT.wav
+echo "E C M is operating in" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\LOOP1.wav
+echo "loop" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\LOOP2.wav
+echo "I will notify you when we reach closed loop" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\LOOP3.wav
+echo "closed" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\CLOSED.wav
+echo "open" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\OPEN.wav
+echo "The engine has been running for" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\RUNTIME1.wav
+echo "minutes" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\RUNTIME2.wav
+echo "Current oxygen sensor reading is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\AIRFUEL1.wav
+echo "volts" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\AIRFUEL2.wav
+echo "Current map sensor reading is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MAP1.wav
+echo "volts" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MAP2.wav
+echo "Engine coolant is at" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\COOLANT1.wav
+echo "degrees celcius" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\COOLANT2.wav
+echo "Current battery sensor reading is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\BATTERY1.wav
+echo "volts" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\BATTERY2.wav
+echo "Current mixture control duty cycle is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MCS1.wav
+echo "Current spark advance is" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\SA1.wav
+echo "degrees before top dead center" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\SA2.wav
+echo "The knock sensor has detected" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\KNOCK1.wav
+echo "knocks since startup" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\KNOCK2.wav
+echo "No trouble codes have been logged in the E C M memory" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF0.wav
+echo "The following trouble codes are logged in the E C M memory" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\MALF\MALF1.wav
+echo "I have calculated" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\CS1.wav
+echo "checksum errors since startup" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\CS2.wav
+echo "Acknowledged" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\ACK.wav
+echo "Point" | c:\windows\syswow64\cscript "C:\program files\jampal\ptts.vbs" -w WAV\POINT.wav
